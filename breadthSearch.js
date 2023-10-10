437b6ce7fb4c7f1f7f9c0179177fd8cf2c894291
Quayle Coral <quayleco@va-b2.kzn.21-school.ru>
2023-10-10 18:16:16 +0300

    delete console.log

--- breadthSearch.js
+++ breadthSearch.js
@@ -6,13 +6,10 @@ function breadthSearch(graph, start, end) {
         if(!graph[key]) {
             graph[key] = [];
         }
-        console.log('\t' + queue);
         if(graph[key].includes(end)) {
             return true;
         } else {
-            console.log('1: ' +queue);
             queue.push(...graph[key]);
-            console.log('2: ' +queue);
         }
     }
     return false;
